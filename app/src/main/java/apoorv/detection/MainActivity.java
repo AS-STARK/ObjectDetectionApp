e521418827dad9f9c30cb8f17380019c775314d5
AS-STARK <apoorvsaran@gmail.com>
2018-04-07 23:08:38 +0530

    initial commit

---
+++ app/src/main/java/apoorv/detection/MainActivity.java
@@ -0,0 +1,29 @@
+package apoorv.detection;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button imgClickBtn = (Button)findViewById(R.id.imgButton);
+        Button pickerBtn = (Button)findViewById(R.id.pickerButton);
+
+        imgClickBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(i);
+            }
+        });
+    }
+
+}
