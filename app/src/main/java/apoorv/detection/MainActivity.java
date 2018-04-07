802495915b26ddfcaaa0b668bcb5937f35dd5505
AS-STARK <apoorvsaran@gmail.com>
2018-04-08 00:03:35 +0530

    image picker working fine

--- app/src/main/java/apoorv/detection/MainActivity.java
+++ app/src/main/java/apoorv/detection/MainActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int imgClick = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +23,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(i);
+                if(i.resolveActivity(getPackageManager()) != null)
+                startActivityForResult(i, imgClick);
             }
         });
+        pickerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent t = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(t, imgClick);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode==imgClick && resultCode==RESULT_OK)
+        {
+            Log.d("TAG", "Image captured");
+        }
     }
 
 }
